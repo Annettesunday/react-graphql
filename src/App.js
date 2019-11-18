15f2550e6eeefb74fe894711cef8263a2310db19
annettesunday <annette.odhiambo@andela.com>
2019-11-18 18:19:29 +0300

    ch(App): connect to backend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import ApolloClient from "apollo-boost";
+import {ApolloProvider} from "react-apollo";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const client = new ApolloClient({
+  uri: "https://vm8mjvrnv3.lp.gql.zone/graphql"
+})
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <h2>My First Apollo React Application</h2>
     </div>
-  );
-}
+  </ApolloProvider>
+)
 
 export default App;
